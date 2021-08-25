09b7b722850c64c70f16fbca69fbece5febc1928
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-08-25 13:05:47 +0300

    🚀 project initialize

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary: "#FF5C00",
+        secondary: "#FFD0B6",
+        tirtiary: "#CFDAFF",
+        footerColor: "#CEB692",
+        backgroundColor:"#FDF7EE",
+      },
+      fontFamily:{
+        body:["Montserrat"],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
